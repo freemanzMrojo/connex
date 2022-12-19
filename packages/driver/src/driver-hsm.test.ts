48b50f7e09a9b4b96d77b5807866db6e8fcfb71a
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2022-12-19 15:25:46 +0100

    jest config

--- packages/driver/src/driver-hsm.test.ts
+++ packages/driver/src/driver-hsm.test.ts
@@ -3,6 +3,6 @@ import { DriverHSM } from './driver-hsm';
 //TODO implement tests
 describe('testing index file', () => {
   test('empty string should result in zero', () => {
-    expect(() => { return 0}).toBe(0);
+    expect(0).toBe(0);
   });
 });
